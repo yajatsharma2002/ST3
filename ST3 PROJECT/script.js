3adad5f6c8c6acde89967e9b74d2dc77d7047553
yajatsharma2002 <109058512+yajatsharma2002@users.noreply.github.com>
2022-09-21 13:26:46 +0530

    Add files via upload

---
+++ ST3 PROJECT/script.js
@@ -0,0 +1,58 @@
+function c1(){
+    let val= document.getElementById("in").value;
+    Number(val);
+    document.getElementById("p1").innerHTML=27.01*val+"kg";
+  }
+  function c2(){
+    let val= document.getElementById("in").value;
+    Number(val);
+    document.getElementById("p1").innerHTML=0.38*val+"kg";
+  }
+
+  function c3(){
+    let val= document.getElementById("in").value;
+    Number(val);
+    document.getElementById("p1").innerHTML=0.91*val+"kg";
+  }
+
+  function c4(){
+    let val= document.getElementById("in").value;
+    Number(val);
+    document.getElementById("p1").innerHTML=val+"kg";
+  }
+
+  function c5(){
+    let val= document.getElementById("in").value;
+    Number(val);
+    document.getElementById("p1").innerHTML=0.38*val+"kg";
+  }
+
+  function c6(){
+    let val= document.getElementById("in").value;
+    Number(val);
+    document.getElementById("p1").innerHTML=2.34*val+"kg";
+  }
+
+  function c7(){
+    let val= document.getElementById("in").value;
+    Number(val);
+    document.getElementById("p1").innerHTML=1.06*val+"kg";
+  }
+
+  function c8(){
+    let val= document.getElementById("in").value;
+    Number(val);
+    document.getElementById("p1").innerHTML=0.92*val+"kg";
+  }
+
+  function c9(){
+    let val= document.getElementById("in").value;
+    Number(val);
+    document.getElementById("p1").innerHTML=1.19*val+"kg";
+  }
+
+  function c10(){
+    let val= document.getElementById("in").value;
+    Number(val);
+    document.getElementById("p1").innerHTML=0.06*val+"kg";
+  }
